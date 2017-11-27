74405585b9e672bbda90a0424d97a142ea7c8908
Chris Oliver <excid3@gmail.com>
2017-11-27 11:41:43 -0600

    Update to latest Stripe gems

--- lib/pay/billable/braintree.rb
+++ lib/pay/billable/braintree.rb
@@ -1,28 +1,18 @@
 module Pay
   module Billable
     module Braintree
-      # def braintree_customer(token = nil)
-      #   if processor_id?
-      #     result = ::Braintree::PaymentMethod.create(
-      #       customer_id: processor_id,
-      #       payment_method_nonce: token,
-      #       options: { make_default: true }
-      #     )
+       def braintree_customer
+         if processor_id?
+           ::Braintree::Customer.find(processor_id)
+         else
+           result = ::Braintree::Customer.create(email: email)
 
-      #     raise StandardError, result.inspect unless result.success?
-      #     ::Braintree::Customer.find(processor_id)
-      #   else
-      #     result = ::Braintree::Customer.create(
-      #       email: email,
-      #       payment_method_nonce: token
-      #     )
+           raise StandardError, result.inspect unless result.success?
+           update(processor: 'braintree', processor_id: result.customer.id)
 
-      #     raise StandardError, result.inspect unless result.success?
-      #     update(processor: 'braintree', processor_id: result.customer.id)
-
-      #     result.customer
-      #   end
-      # end
+           result.customer
+         end
+       end
 
       # def create_braintree_subscription(name = 'default')
       #   token = braintree_customer.payment_methods.find(&:default?).token
