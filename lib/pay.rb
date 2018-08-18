6276e2490f945469930f4e46bf8cacff05f50dde
Jason Charnes <jason@thecharnes.com>
2018-08-18 17:17:07 -0500

    Update gem for local testing

--- lib/pay.rb
+++ lib/pay.rb
@@ -2,14 +2,15 @@ require 'braintree'
 require 'stripe'
 require 'pay/engine'
 require 'pay/billable'
-require 'pay/stripe/charge_succeeded'
-require 'pay/stripe/charge_refunded'
+require_relative 'pay/stripe/charge_succeeded'
+require_relative 'pay/stripe/charge_refunded'
 
 module Pay
   # Define who owns the subscription
   mattr_accessor :billable_class
   mattr_accessor :billable_table
   mattr_accessor :braintree_gateway
+
   @@billable_class = 'User'
   @@billable_table = @@billable_class.tableize
 
