803a95207c5f237426605661aabb26405865b66f
Chris Oliver <excid3@gmail.com>
2018-09-14 17:10:04 -0500

    Pull out charge methods into a concern (#21)

---
+++ app/models/concerns/pay/chargeable.rb
@@ -0,0 +1,38 @@
+module Pay
+  module Chargeable
+    extend ActiveSupport::Concern
+
+    included do
+      # Associations
+      belongs_to :owner, class_name: Pay.billable_class, foreign_key: :owner_id
+
+      # Validations
+      validates :amount, presence: true
+      validates :processor, presence: true
+      validates :processor_id, presence: true
+      validates :card_type, presence: true
+    end
+
+    def processor_charge
+      send("#{processor}_charge")
+    end
+
+    def refund!(amount = nil)
+      send("#{processor}_refund!", amount)
+    end
+
+    def stripe_charge
+      Stripe::Charge.retrieve(processor_id)
+    end
+
+    def stripe_refund!(amount)
+      Stripe::Refund.create(
+        charge: processor_id,
+        amount: amount
+      )
+
+      update(amount_refunded: amount)
+    end
+
+  end
+end
