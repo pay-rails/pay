ce6a07edf0415d313edd3723a17e28e4bd7aadbd
Chris Oliver <excid3@gmail.com>
2018-12-19 18:46:54 -0600

    Add method to lookup processor charge directly from charge object

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -2,4 +2,13 @@ class Charge < ApplicationRecord
   include Pay::Chargeable
 
   validates :processor_id, uniqueness: { scope: :processor }
+
+  def processor_charge
+    case processor
+    when "stripe"
+      ::Stripe::Charge.retrieve(processor_id)
+    when "braintree"
+      Pay.braintree_gateway.transactions.find(processor_id)
+    end
+  end
 end
