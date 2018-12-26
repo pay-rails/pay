b91387ce333b7287b197533189c48274032e3ec6
Jesper Christiansen <hi@jespr.com>
2018-12-26 11:02:01 -0700

    Added simple Pay::UserMailer tests

---
+++ test/mailers/pay/user_mailer_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+ 
+class UserMailerTest < ActionMailer::TestCase
+  test "receipt" do
+    user = User.new(email: 'john@example.org')
+    user.stubs(:extra_billing_info?).returns(true)
+    user.stubs(:extra_billing_info).returns('extra billing info')
+    charge = Charge.new(amount: 100, owner: user)
+    receipt = mock('receipt')
+    receipt.stubs(:render).returns('render content')
+    charge.stubs(:filename).returns('file.name')
+    charge.stubs(:receipt).returns(receipt)
+    email = Pay::UserMailer.receipt(user, charge)
+ 
+    assert_equal ['john@example.org'], email.to
+    assert_equal 'Payment receipt', email.subject
+
+    # Configurable subject
+    new_subject = 'My email receipt subject'
+    Pay.email_receipt_subject = new_subject
+    email = Pay::UserMailer.receipt(user, charge)
+    assert_equal new_subject, email.subject
+  end
+
+  test "refund" do
+    user = User.new(email: 'john@example.org')
+    user.stubs(:extra_billing_info?).returns(true)
+    user.stubs(:extra_billing_info).returns('extra billing info')
+    charge = Charge.new(amount: 100, owner: user)
+    receipt = mock('receipt')
+    receipt.stubs(:render).returns('render content')
+    charge.stubs(:filename).returns('file.name')
+    charge.stubs(:receipt).returns(receipt)
+    email = Pay::UserMailer.refund(user, charge)
+ 
+    assert_equal ['john@example.org'], email.to
+    assert_equal 'Payment refunded', email.subject
+
+    # Configurable subject
+    new_subject = 'My email refund subject'
+    Pay.email_refund_subject = new_subject
+    email = Pay::UserMailer.refund(user, charge)
+    assert_equal new_subject, email.subject
+  end
+
+  test "renewal" do
+    user = User.new(email: 'john@example.org')
+    user.stubs(:extra_billing_info?).returns(true)
+    user.stubs(:extra_billing_info).returns('extra billing info')
+    charge = Charge.new(amount: 100, owner: user)
+    receipt = mock('receipt')
+    receipt.stubs(:render).returns('render content')
+    charge.stubs(:filename).returns('file.name')
+    charge.stubs(:receipt).returns(receipt)
+    email = Pay::UserMailer.subscription_renewing(user, charge)
+ 
+    assert_equal ['john@example.org'], email.to
+    assert_equal 'Your upcoming subscription renewal', email.subject
+
+    # Configurable subject
+    new_subject = 'My email renewal subject'
+    Pay.email_renewing_subject = new_subject
+    email = Pay::UserMailer.subscription_renewing(user, charge)
+    assert_equal new_subject, email.subject
+  end
+end
