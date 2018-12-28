3ac27ab3bd665f2581c7cafb751a10d1e9657da1
Jesper Christiansen <hi@jespr.com>
2018-12-27 18:31:34 -0700

    Added email views generator

---
+++ lib/generators/pay/email_views_generator.rb
@@ -0,0 +1,13 @@
+require 'rails/generators'
+
+module Pay
+  module Generators
+    class EmailViewsGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../../..", __FILE__)
+
+      def copy_views
+        directory 'app/views/pay/user_mailer', 'app/views/pay/user_mailer'
+      end
+    end
+  end
+end
