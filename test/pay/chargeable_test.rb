d8451ed17a04ab629cf81e1e4aacc1ecf462d10e
Chris Oliver <excid3@gmail.com>
2018-12-30 19:38:21 -0600

    Remove receipt test for now

--- test/pay/chargeable_test.rb
+++ test/pay/chargeable_test.rb
@@ -14,20 +14,4 @@ class Pay::Chargeable::Test < ActiveSupport::TestCase
     @chargeable.card_last4 = 1234
     assert_equal "VISA (**** **** **** 1234)", @chargeable.charged_to
   end
-
-  test '#receipt is only available if Receipts::Receipt is defined' do
-    refute @chargeable.respond_to?(:receipt)
-
-    module Receipts
-      def receipt; end
-    end
-
-    class MyCharge < Charge
-      include Pay::Chargeable
-      include Receipts
-    end
-
-    mychargeable = MyCharge.new
-    assert mychargeable.respond_to?(:receipt)
-  end
 end
