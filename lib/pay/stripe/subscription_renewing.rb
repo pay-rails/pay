2d6e2685c4b64b144b0c86ce3f52392dff9641df
Jesper Christiansen <hi@jespr.com>
2019-01-02 21:41:47 -0800

    Use event.data.object.subscription instead of object.id for renewing webhook

--- lib/pay/stripe/subscription_renewing.rb
+++ lib/pay/stripe/subscription_renewing.rb
@@ -2,8 +2,12 @@ module Pay
   module Stripe
     class SubscriptionRenewing
       def call(event)
-        object = event.data.object
-        subscription = ::Subscription.find_by(processor: :stripe, processor_id: object.id)
+        # Event is of type "invoice" see:
+        # https://stripe.com/docs/api/invoices/object
+        subscription = ::Subscription.find_by(
+          processor: :stripe,
+          processor_id: event.data.object.subscription
+        )
         notify_user(subscription.user, subscription) if subscription.present?
       end
 
