4873469225a9d0b2cfca473daadd168a17271c58
Chris Oliver <excid3@gmail.com>
2019-01-14 02:10:34 -0600

    Use ActiveSupport Reloader to continuously include modules on reloads

--- lib/pay/engine.rb
+++ lib/pay/engine.rb
@@ -4,31 +4,32 @@ module Pay
 
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
-
     initializer 'pay.processors' do
-      # Include processor backends
-      if defined? Stripe
-        Dir[File.join(__dir__, 'pay/stripe', '**', '*.rb')].each { |file| require file }
+      ActiveSupport::Reloader.to_prepare do
+        # Include processor backends
+        if defined? Stripe
+          Dir[File.join(__dir__, 'pay/stripe', '**', '*.rb')].each { |file| require file }
 
-        Pay.charge_model.include Pay::Stripe::Charge
-        Pay.subscription_model.include Pay::Stripe::Subscription
-        Pay.user_model.include Pay::Stripe::Billable
+          Pay.charge_model.include Pay::Stripe::Charge
+          Pay.subscription_model.include Pay::Stripe::Subscription
+          Pay.user_model.include Pay::Stripe::Billable
 
-        Pay::Stripe::Api.set_api_keys
-      end
+          Pay::Stripe::Api.set_api_keys
+        end
 
-      if defined? Braintree
-        Dir[File.join(__dir__, 'pay/braintree', '**', '*.rb')].each { |file| require file }
+        if defined? Braintree
+          Dir[File.join(__dir__, 'pay/braintree', '**', '*.rb')].each { |file| require file }
 
-        Pay.charge_model.include Pay::Braintree::Charge
-        Pay.subscription_model.include Pay::Braintree::Subscription
-        Pay.user_model.include Pay::Braintree::Billable
+          Pay.charge_model.include Pay::Braintree::Charge
+          Pay.subscription_model.include Pay::Braintree::Subscription
+          Pay.user_model.include Pay::Braintree::Billable
 
-        Pay::Braintree::Api.set_api_keys
-      end
+          Pay::Braintree::Api.set_api_keys
+        end
 
-      if defined?(Receipts::Receipt)
-        Pay.charge_model.include Pay::Receipts
+        if defined?(Receipts::Receipt)
+          Pay.charge_model.include Pay::Receipts
+        end
       end
     end
   end
