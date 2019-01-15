6ea7f6a026d3ca34ceacdcdfa9bc7c9b901dddb1
Chris Oliver <excid3@gmail.com>
2019-01-14 21:16:27 -0600

    Put setup into methods

--- lib/pay/engine.rb
+++ lib/pay/engine.rb
@@ -3,10 +3,13 @@ module Pay
     isolate_namespace Pay
 
     initializer 'pay.processors' do
+      # Include processor backends
+      require 'pay/stripe'    if defined? ::Stripe
+      require 'pay/braintree' if defined? ::Braintree
+
       ActiveSupport::Reloader.to_prepare do
-        # Include processor backends
-        require 'pay/stripe'    if defined? ::Stripe
-        require 'pay/braintree' if defined? ::Braintree
+        Pay::Stripe.setup    if defined? ::Stripe
+        Pay::Braintree.setup if defined? ::Braintree
 
         if defined?(Receipts::Receipt)
           Pay.charge_model.include Pay::Receipts
