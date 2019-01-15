50e4cb733b9cbc90dabffa9150f31f1c2b6ef549
Chris Oliver <excid3@gmail.com>
2019-01-14 21:16:27 -0600

    Put setup into methods

--- lib/pay/stripe.rb
+++ lib/pay/stripe.rb
@@ -4,8 +4,14 @@ require 'pay/stripe/charge'
 require 'pay/stripe/subscription'
 require 'pay/stripe/webhooks'
 
-Pay::Stripe::Api.set_api_keys
+module Pay
+  module Stripe
+    def self.setup
+      Pay::Stripe::Api.set_api_keys
 
-Pay.charge_model.include Pay::Stripe::Charge
-Pay.subscription_model.include Pay::Stripe::Subscription
-Pay.user_model.include Pay::Stripe::Billable
+      Pay.charge_model.include Pay::Stripe::Charge
+      Pay.subscription_model.include Pay::Stripe::Subscription
+      Pay.user_model.include Pay::Stripe::Billable
+    end
+  end
+end
