11704e64c7c9cbcd2a8fe9e5cd62634cbc2be54a
Chris Oliver <excid3@gmail.com>
2019-01-14 21:20:09 -0600

    Add braintree setup method

--- lib/pay/braintree.rb
+++ lib/pay/braintree.rb
@@ -3,8 +3,14 @@ require 'pay/braintree/billable'
 require 'pay/braintree/charge'
 require 'pay/braintree/subscription'
 
-Pay::Braintree::Api.set_api_keys
+module Pay
+  module Braintree
+    def self.setup
+      Pay::Braintree::Api.set_api_keys
 
-Pay.charge_model.include Pay::Braintree::Charge
-Pay.subscription_model.include Pay::Braintree::Subscription
-Pay.user_model.include Pay::Braintree::Billable
+      Pay.charge_model.include Pay::Braintree::Charge
+      Pay.subscription_model.include Pay::Braintree::Subscription
+      Pay.user_model.include Pay::Braintree::Billable
+    end
+  end
+end
