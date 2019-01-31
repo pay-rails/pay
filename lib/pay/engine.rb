6e29c77b3c303e3e1285ecfc44a12669ab3c514f
Chris Oliver <excid3@gmail.com>
2019-01-30 21:37:17 -0600

    Don't need isolating of namespace

--- lib/pay/engine.rb
+++ lib/pay/engine.rb
@@ -1,7 +1,5 @@
 module Pay
   class Engine < ::Rails::Engine
-    isolate_namespace Pay
-
     initializer 'pay.processors' do
       # Include processor backends
       require 'pay/stripe'    if defined? ::Stripe
