59482b04dae3940bf11382e71b2f902cfab0a20b
Chris Oliver <excid3@gmail.com>
2019-02-07 00:05:12 -0600

    Fix reloader problems

--- lib/pay/engine.rb
+++ lib/pay/engine.rb
@@ -4,14 +4,14 @@ module Pay
       # Include processor backends
       require 'pay/stripe'    if defined? ::Stripe
       require 'pay/braintree' if defined? ::Braintree
+    end
 
-      ActiveSupport::Reloader.to_prepare do
-        Pay::Stripe.setup    if defined? ::Stripe
-        Pay::Braintree.setup if defined? ::Braintree
+    config.to_prepare do
+      Pay::Stripe.setup    if defined? ::Stripe
+      Pay::Braintree.setup if defined? ::Braintree
 
-        if defined?(Receipts::Receipt)
-          Pay.charge_model.include Pay::Receipts
-        end
+      if defined?(Receipts::Receipt)
+        Pay.charge_model.include Pay::Receipts
       end
     end
   end
