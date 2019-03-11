f80db354d90fc66babb65f694711b6e016e6a580
jean-francois labbe <labbe.jean.francois@gmail.com>
2019-03-11 17:05:03 +0100

    give env variables higher precedence (#74)

--- lib/pay/stripe/api.rb
+++ lib/pay/stripe/api.rb
@@ -6,7 +6,7 @@ module Pay
         secrets     = Rails.application.secrets
         credentials = Rails.application.credentials
 
-        ::Stripe.api_key = secrets.dig(env, :stripe, :private_key) || credentials.dig(env, :stripe, :private_key) || ENV["STRIPE_PRIVATE_KEY"]
+        ::Stripe.api_key = ENV["STRIPE_PRIVATE_KEY"] || secrets.dig(env, :stripe, :private_key) || credentials.dig(env, :stripe, :private_key)
       end
     end
   end
