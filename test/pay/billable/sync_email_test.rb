abc6e7e12baead758db017f7411da039ab0eb628
Chris Oliver <excid3@gmail.com>
2020-01-06 11:44:19 -0600

    Improve email sync tests

--- test/pay/billable/sync_email_test.rb
+++ test/pay/billable/sync_email_test.rb
@@ -1,16 +1,23 @@
 require 'test_helper'
 
 class Pay::Billable::SyncEmail::Test < ActiveSupport::TestCase
-  setup do
-    @billable = User.new
-  end
+  include ActiveJob::TestHelper
 
-  test 'email sync' do
-    assert @billable.should_sync_email_with_processor?
+  test 'email sync only on updating customer email' do
+    billable = User.new(email: "test@example.com", processor_id: "test")
+
+    assert_no_enqueued_jobs do
+      billable.save
+    end
+
+    assert_enqueued_jobs 1 do
+      billable.update(email: "test@test.com")
+    end
   end
 
   test 'email sync should be ignored for billable that delegates email' do
-    billable = Team.create(name: "Team 1")
-    refute billable.should_sync_email_with_processor?
+    assert_no_enqueued_jobs do
+      billable = Team.create(name: "Team 1")
+    end
   end
 end
