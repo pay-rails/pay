ebcc15f016796b14b51dd79fe71521efc59295b7
Chris Oliver <excid3@gmail.com>
2020-06-05 17:53:31 -0500

    Update 20170727235816_create_pay_charges.rb

--- db/migrate/20170727235816_create_pay_charges.rb
+++ db/migrate/20170727235816_create_pay_charges.rb
@@ -1,8 +1,8 @@
 class CreatePayCharges < ActiveRecord::Migration[4.2]
   def change
     create_table :pay_charges do |t|
-      # Some Billable objects use string as ID, change type if that is a case.
-      t.references :owner, polymorphic: true, type: :integer
+      # Some Billable objects use string as ID, add `type: :string` if needed
+      t.references :owner, polymorphic: true
       t.string :processor, null: false
       t.string :processor_id, null: false
       t.integer :amount, null: false
