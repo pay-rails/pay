8083e81f339a155b06c7b653ffdb3503ce020c27
Dr Nic Williams <drnicwilliams@gmail.com>
2020-09-01 14:29:38 +1000

    allow pay 'Go back' button to be configured via ?back=/path/to/item

--- app/controllers/pay/payments_controller.rb
+++ app/controllers/pay/payments_controller.rb
@@ -1,6 +1,7 @@
 module Pay
   class PaymentsController < ApplicationController
     def show
+      @redirect_to = params[:back].presence || root_path
       @payment = Payment.from_id(params[:id])
     end
   end
