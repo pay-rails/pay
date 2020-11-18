b90a02e5bfbeaee89a9bf9954c37d7b5397e1a17
Chris Oliver <excid3@gmail.com>
2020-11-18 10:46:00 -0600

    Test against multiple databases in GitHub Actions

--- test/dummy/app/models/application_record.rb
+++ test/dummy/app/models/application_record.rb
@@ -1,6 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  database = ENV.fetch("DB", "postgresql").to_sym
-  connects_to database: { writing: database, reading: database }
 end
