a80606c49d7ad35745acd7ceee67aa78732f2c36
Nicolas Metzger <nico.metzger@gmail.com>
2020-12-09 16:41:06 +0100

    Add payment details to paddle charges

--- test/pay/paddle/webhooks/subscription_payment_succeeded_test.rb
+++ test/pay/paddle/webhooks/subscription_payment_succeeded_test.rb
@@ -9,6 +9,7 @@ class Pay::Paddle::Webhooks::SubscriptionPaymentSucceededTest < ActiveSupport::T
     @user = User.create!(email: "gob@bluth.com", processor: :paddle, processor_id: @data["user_id"])
 
     assert_difference "Pay.charge_model.count" do
+      PaddlePay::Subscription::User.stubs(:list).returns(:nil)
       Pay::Paddle::Webhooks::SubscriptionPaymentSucceeded.new(@data)
     end
 
@@ -16,6 +17,66 @@ class Pay::Paddle::Webhooks::SubscriptionPaymentSucceededTest < ActiveSupport::T
     assert_equal Integer(@data["sale_gross"].to_f * 100), charge.amount
     assert_equal @data["payment_method"], charge.card_type
     assert_equal @data["receipt_url"], charge.paddle_receipt_url
+  end
+
+  test "a charge is created and card details are set" do
+    @user = User.create!(email: "gob@bluth.com", processor: :paddle, processor_id: @data["user_id"])
+
+    subscription_user = {
+      subscription_id: 7654321,
+      plan_id: 123456,
+      user_id: 12345678,
+      user_email: "test@example.com",
+      marketing_consent: false,
+      update_url: "https://example.com",
+      cancel_url: "https://example.com",
+      state: "active",
+      signup_date: "2020-12-08 07:52:22",
+      last_payment: {amount: 0, currency: "USD", date: "2020-12-08"}, linked_subscriptions: [],
+      payment_information: {payment_method: "card", card_type: "visa", last_four_digits: "0020", expiry_date: "06/2022"},
+      next_payment: {amount: 0, currency: "USD", date: "2021-01-08"}
+    }
+
+    assert_difference "Pay.charge_model.count" do
+      PaddlePay::Subscription::User.stubs(:list).returns([subscription_user])
+      Pay::Paddle::Webhooks::SubscriptionPaymentSucceeded.new(@data)
+    end
+
+    charge = Pay.charge_model.last
+    assert_equal "visa", charge.card_type
+    assert_equal "0020", charge.card_last4
+    assert_equal "06", charge.card_exp_month
+    assert_equal "2022", charge.card_exp_year
+  end
+
+  test "a charge is created and paypal details are set" do
+    @user = User.create!(email: "gob@bluth.com", processor: :paddle, processor_id: @data["user_id"])
+
+    subscription_user = {
+      subscription_id: 7654321,
+      plan_id: 123456,
+      user_id: 12345678,
+      user_email: "test@example.com",
+      marketing_consent: false,
+      update_url: "https://example.com",
+      cancel_url: "https://example.com",
+      state: "active",
+      signup_date: "2020-12-08 07:52:22",
+      last_payment: {amount: 0, currency: "USD", date: "2020-12-08"}, linked_subscriptions: [],
+      payment_information: {payment_method: "paypal"},
+      next_payment: {amount: 0, currency: "USD", date: "2021-01-08"}
+    }
+
+    assert_difference "Pay.charge_model.count" do
+      PaddlePay::Subscription::User.stubs(:list).returns([subscription_user])
+      Pay::Paddle::Webhooks::SubscriptionPaymentSucceeded.new(@data)
+    end
+
+    charge = Pay.charge_model.last
+    assert_equal "PayPal", charge.card_type
+    assert_nil charge.card_last4
+    assert_nil charge.card_exp_month
+    assert_nil charge.card_exp_year
   end
 
   test "a charge isn't created if no corresponding user can be found" do
