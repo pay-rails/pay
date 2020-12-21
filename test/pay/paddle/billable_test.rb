b1f9f54b6eb95f17e1fa4d640ba6c320666da769
Nicolas Metzger <nico.metzger@gmail.com>
2020-12-21 17:49:01 +0100

    Sync current payment information from paddle

--- test/pay/paddle/billable_test.rb
+++ test/pay/paddle/billable_test.rb
@@ -26,4 +26,29 @@ class Pay::Paddle::Billable::Test < ActiveSupport::TestCase
     subscription = ::PaddlePay::Subscription::User.list({subscription_id: "3576390"}, {}).try(:first)
     assert_equal @billable.paddle_subscription("3576390").subscription_id, subscription[:subscription_id]
   end
+
+  test "paddle can sync payment information" do
+    subscription_user = {
+      subscription_id: 7654321,
+      plan_id: 123456,
+      user_id: 12345678,
+      user_email: "test@example.com",
+      marketing_consent: false,
+      update_url: "https://example.com",
+      cancel_url: "https://example.com",
+      state: "active",
+      signup_date: "2020-12-08 07:52:22",
+      last_payment: {amount: 0, currency: "USD", date: "2020-12-08"}, linked_subscriptions: [],
+      payment_information: {payment_method: "card", card_type: "visa", last_four_digits: "0020", expiry_date: "06/2022"},
+      next_payment: {amount: 0, currency: "USD", date: "2021-01-08"}
+    }
+    PaddlePay::Subscription::User.stubs(:list).returns([subscription_user])
+
+    @billable.sync_payment_information_from_paddle
+
+    assert_equal "visa", @billable.card_type
+    assert_equal "0020", @billable.card_last4
+    assert_equal "06", @billable.card_exp_month
+    assert_equal "2022", @billable.card_exp_year
+  end
 end
