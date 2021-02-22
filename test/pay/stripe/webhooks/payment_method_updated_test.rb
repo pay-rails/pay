2fa6bd06c3c2fe96e1d315ca37ccce3d80ad6af5
Chris Oliver <excid3@gmail.com>
2021-02-22 11:13:46 -0600

    Fixes #279

--- test/pay/stripe/webhooks/payment_method_updated_test.rb
+++ test/pay/stripe/webhooks/payment_method_updated_test.rb
@@ -1,16 +1,16 @@
 require "test_helper"
 
-class Pay::Stripe::Webhooks::CustomerUpdatedTest < ActiveSupport::TestCase
+class Pay::Stripe::Webhooks::PaymentMethodUpdatedtest < ActiveSupport::TestCase
   setup do
     @event = OpenStruct.new
-    @event.data = JSON.parse(File.read("test/support/fixtures/stripe/customer_updated_event.json"), object_class: OpenStruct)
+    @event.data = JSON.parse(File.read("test/support/fixtures/stripe/payment_method.updated.json"), object_class: OpenStruct)
   end
 
   test "update_card_from stripe is called upon customer update" do
     user = User.create!(
       email: "gob@bluth.com",
       processor: :stripe,
-      processor_id: @event.data.object.id
+      processor_id: @event.data.object.customer
     )
     user.subscriptions.create!(
       processor: :stripe,
@@ -21,7 +21,7 @@ class Pay::Stripe::Webhooks::CustomerUpdatedTest < ActiveSupport::TestCase
     )
 
     Pay::Stripe::Billable.any_instance.expects(:sync_card_from_stripe)
-    Pay::Stripe::Webhooks::CustomerUpdated.new.call(@event)
+    Pay::Stripe::Webhooks::PaymentMethodUpdated.new.call(@event)
   end
 
   test "update_card_from stripe is not called if user can't be found" do
@@ -39,6 +39,7 @@ class Pay::Stripe::Webhooks::CustomerUpdatedTest < ActiveSupport::TestCase
     )
 
     Pay::Stripe::Billable.any_instance.expects(:sync_card_from_stripe).never
-    Pay::Stripe::Webhooks::CustomerUpdated.new.call(@event)
+    Pay::Stripe::Webhooks::PaymentMethodUpdated.new.call(@event)
   end
 end
+
