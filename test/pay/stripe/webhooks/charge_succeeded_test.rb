c75434c6db316cc18fbb80e6de01073c0135fc20
Chris Oliver <excid3@gmail.com>
2021-05-18 21:04:38 -0500

    Fix test

--- test/pay/stripe/webhooks/charge_succeeded_test.rb
+++ test/pay/stripe/webhooks/charge_succeeded_test.rb
@@ -4,10 +4,11 @@ class Pay::Stripe::Webhooks::ChargeSucceededTest < ActiveSupport::TestCase
   setup do
     @event = OpenStruct.new
     @event.data = JSON.parse(File.read("test/support/fixtures/stripe/charge_succeeded_event.json"), object_class: OpenStruct)
-    @user = User.create!(email: "gob@bluth.com", processor: :stripe, processor_id: @event.data.object.customer)
   end
 
   test "a charge is created" do
+    @user = User.create!(email: "gob@bluth.com", processor: :stripe, processor_id: @event.data.object.customer)
+
     assert_difference "Pay.charge_model.count" do
       Pay::Stripe::Webhooks::ChargeSucceeded.new.call(@event)
     end
@@ -28,6 +29,7 @@ class Pay::Stripe::Webhooks::ChargeSucceededTest < ActiveSupport::TestCase
   end
 
   test "a charge isn't created if it already exists" do
+    @user = User.create!(email: "gob@bluth.com", processor: :stripe, processor_id: @event.data.object.customer)
     @user.charges.create!(amount: 100, processor: :stripe, processor_id: "ch_chargeid", card_type: "Visa", card_exp_month: 1, card_exp_year: 2019, card_last4: "4444")
 
     assert_no_difference "Pay.charge_model.count" do
@@ -36,6 +38,7 @@ class Pay::Stripe::Webhooks::ChargeSucceededTest < ActiveSupport::TestCase
   end
 
   test "stripe charge gets associated with subscription" do
+    @user = User.create!(email: "gob@bluth.com", processor: :stripe, processor_id: @event.data.object.customer)
     subscription = @user.subscriptions.create(name: "default", processor: :stripe, processor_id: "12345", processor_plan: "default", quantity: 1, status: "active")
     invoice = OpenStruct.new(subscription: "12345")
     charge = OpenStruct.new(
