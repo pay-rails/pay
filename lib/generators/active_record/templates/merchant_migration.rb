98a81f5da476524a07e93f6cf293b5541e1216e0
Chris Oliver <excid3@gmail.com>
2021-06-17 09:25:27 -0500

    Fix typo in merchant migration. Closes #357

--- lib/generators/active_record/templates/merchant_migration.rb
+++ lib/generators/active_record/templates/merchant_migration.rb
@@ -6,7 +6,7 @@ class AddPayMerchantTo<%= table_name.camelize %> < ActiveRecord::Migration<%= mi
 
     # We may already have the pay_data column if a Pay::Billable object is also a Pay::Merchant
     unless ActiveRecord::Base.connection.column_exists?(table_name, :pay_data)
-      add_column table_name, :pay_data, Pay::Adapter.json_column_type
+      add_column :<%= table_name %>, :pay_data, Pay::Adapter.json_column_type
     end
   end
 end
