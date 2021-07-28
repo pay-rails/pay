d2ba81194501eebc9435f337fbffaba1f3246633
Chris Oliver <excid3@gmail.com>
2021-07-28 10:11:18 -0500

    Remove pay model

--- app/models/pay.rb
+++
@@ -1,5 +0,0 @@
-module Pay
-  def self.table_name_prefix
-    "pay_"
-  end
-end
--- app/models/pay/application_record.rb
+++ app/models/pay/application_record.rb
@@ -1,6 +1,7 @@
 module Pay
   class ApplicationRecord < Pay.model_parent_class.constantize
     self.abstract_class = true
+    self.table_name_prefix = "pay_"
 
     def self.json_column?(name)
       return unless connected? && table_exists?
