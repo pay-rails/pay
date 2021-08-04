373acc90ddb6eb7cc7bbea9d148ada3c50617e2d
Nathan Jones <nate@mrjones.io>
2021-08-04 11:12:17 -0500

    support options in subscription#sync

--- lib/pay/stripe/subscription.rb
+++ lib/pay/stripe/subscription.rb
@@ -20,9 +20,15 @@ module Pay
         :trial_ends_at,
         to: :pay_subscription
 
-      def self.sync(subscription_id, object: nil, name: Pay.default_product_name, try: 0, retries: 1)
+      def self.sync(subscription_id, object: nil, options: {}, name: Pay.default_product_name, try: 0, retries: 1)
         # Skip loading the latest subscription details from the API if we already have it
-        object ||= ::Stripe::Subscription.retrieve({id: subscription_id, expand: ["pending_setup_intent", "latest_invoice.payment_intent"]})
+        object ||= ::Stripe::Subscription.retrieve(
+          {
+            id: subscription_id,
+            expand: ["pending_setup_intent", "latest_invoice.payment_intent"],
+          },
+          { stripe_account: options[:stripe_account] }
+        )
 
         owner = Pay.find_billable(processor: :stripe, processor_id: object.customer)
         return unless owner
