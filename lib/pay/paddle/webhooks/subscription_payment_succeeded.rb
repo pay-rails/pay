457824c4febda30deee52e3a58b7b02ef34d99b9
Chris Oliver <excid3@gmail.com>
2021-08-16 13:40:37 -0500

    Fix paddle metadata

--- lib/pay/paddle/webhooks/subscription_payment_succeeded.rb
+++ lib/pay/paddle/webhooks/subscription_payment_succeeded.rb
@@ -30,7 +30,7 @@ module Pay
             currency: event["currency"],
             paddle_receipt_url: event["receipt_url"],
             subscription: pay_customer.subscriptions.find_by(processor_id: event["subscription_id"]),
-            metadata: Pay::Paddle.parse_passthrough(event["passthrough"].except("owner_sgid"))
+            metadata: Pay::Paddle.parse_passthrough(event["passthrough"]).except("owner_sgid")
           }.merge(payment_method_details)
 
           charge = pay_customer.charges.find_or_initialize_by(processor_id: event["subscription_payment_id"])
