2297aa6b6a7223929b0ea35ffaa719d4f0a111a3
Chris Oliver <excid3@gmail.com>
2021-08-16 08:42:32 -0500

    standardize

--- lib/pay/nano_id.rb
+++ lib/pay/nano_id.rb
@@ -4,10 +4,9 @@ module Pay
     ALPHABET = "_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".freeze
     ALPHABET_SIZE = ALPHABET.size
 
-
     def self.generate(size: 21)
       id = ""
-      size.times{ id << ALPHABET[(Random.rand * ALPHABET_SIZE).floor] }
+      size.times { id << ALPHABET[(Random.rand * ALPHABET_SIZE).floor] }
       id
     end
   end
