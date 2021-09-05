b42fd950c4b0e2cc49553e3300f9045ed1bbd930
Chris Oliver <excid3@gmail.com>
2021-09-04 19:50:10 -0500

    Use different line items for invoices

--- lib/pay/receipts.rb
+++ lib/pay/receipts.rb
@@ -16,6 +16,16 @@ module Pay
     end
 
     def receipt_pdf(**options)
+      line_items = [
+        [I18n.t("pay.receipt.date"), I18n.l(created_at, format: :long)],
+        [I18n.t("pay.receipt.account_billed"), "#{customer.customer_name} (#{customer.email})"],
+        [I18n.t("pay.receipt.product"), product],
+        [I18n.t("pay.receipt.amount"), number_to_currency(amount / 100.0)],
+        [I18n.t("pay.receipt.charged_to"), charged_to]
+      ]
+      line_items << [I18n.t("pay.receipt.additional_info"), customer.owner.extra_billing_info] if customer.owner.extra_billing_info?
+      line_items << [I18n.t("pay.receipt.refunded"), number_to_currency(amount_refunded / 100.0)] if refunded?
+
       defaults = {
         id: id,
         product: product,
@@ -40,15 +50,25 @@ module Pay
     end
 
     def invoice_pdf(**options)
+      bill_to = [owner.name]
+      bill_to += [owner.extra_billing_info] if owner.extra_billing_info?
+      bill_to += [nil, owner.email]
+
+      total = ActionController::Base.helpers.number_to_currency(amount / 100.0)
+
+      line_items = [
+        ["<b>#{I18n.t("pay.invoice.product")}</b>", nil, "<b>#{I18n.t("pay.invoice.amount")}</b>"],
+        [product, nil, total],
+        [nil, I18n.t("pay.invoice.subtotal"), total],
+        [nil, I18n.t("pay.invoice.total"), total]
+      ]
+
       defaults = {
         id: id,
         issue_date: I18n.l(created_at, format: :long),
         due_date: I18n.l(created_at, format: :long),
         status: "<b><color rgb='#5eba7d'>#{I18n.t("pay.receipt.paid").upcase}</color></b>",
-        bill_to: [
-          customer.customer_name,
-          customer.email
-        ].compact,
+        bill_to: bill_to,
         product: product,
         company: {
           name: Pay.business_name,
@@ -63,15 +83,7 @@ module Pay
     end
 
     def line_items
-      line_items = [
-        [I18n.t("pay.receipt.date"), I18n.l(created_at, format: :long)],
-        [I18n.t("pay.receipt.account_billed"), "#{customer.customer_name} (#{customer.email})"],
-        [I18n.t("pay.receipt.product"), product],
-        [I18n.t("pay.receipt.amount"), number_to_currency(amount / 100.0)],
-        [I18n.t("pay.receipt.charged_to"), charged_to]
-      ]
-      line_items << [I18n.t("pay.receipt.additional_info"), customer.owner.extra_billing_info] if customer.owner.extra_billing_info?
-      line_items << [I18n.t("pay.receipt.refunded"), number_to_currency(amount_refunded / 100.0)] if refunded?
+
       line_items
     end
   end
