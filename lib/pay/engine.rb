74f43cb144c948f69357e637deed0517113bb762
Vlad Platon <platonnvlad@gmail.com>
2021-10-27 14:39:48 +0300

    Setup processors based on enabled_processors config
    Signed-off-by: Vlad Platon <platonnvlad@gmail.com>

--- lib/pay/engine.rb
+++ lib/pay/engine.rb
@@ -18,9 +18,9 @@ module Pay
     end
 
     config.to_prepare do
-      Pay::Stripe.setup if defined? ::Stripe
-      Pay::Braintree.setup if defined? ::Braintree
-      Pay::Paddle.setup if defined? ::PaddlePay
+      Pay::Stripe.setup if defined? ::Stripe && Pay.enabled_processors&.include?(:stripe)
+      Pay::Braintree.setup if defined? ::Braintree && Pay.enabled_processors&.include?(:braintree)
+      Pay::Paddle.setup if defined? ::PaddlePay && Pay.enabled_processors&.include?(:paddle)
 
       Pay::Charge.include Pay::Receipts if defined? ::Receipts::Receipt
     end
