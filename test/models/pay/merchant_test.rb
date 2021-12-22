eea770999a24585dce71ad3b592e03989058d540
olimart <olimart@users.noreply.github.com>
2021-12-21 20:51:09 -0500

    Add onboarding status check

--- test/models/pay/merchant_test.rb
+++ test/models/pay/merchant_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class Pay::MerchantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return stripe connect onboarding status" do
+    merchant = Pay::Merchant.new
+    refute merchant.onboarding_complete?
+
+    merchant.onboarding_complete = true
+    assert merchant.onboarding_complete?
+  end
 end
