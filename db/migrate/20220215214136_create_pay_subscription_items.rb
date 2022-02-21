e88d049a6ed38b3433c1609030bfc7c21a458411
Nathan Jones <nate@mrjones.io>
2022-02-21 07:20:44 -0600

    subscription item migration must be -v 6.0

--- db/migrate/20220215214136_create_pay_subscription_items.rb
+++ db/migrate/20220215214136_create_pay_subscription_items.rb
@@ -1,4 +1,4 @@
-class CreatePaySubscriptionItems < ActiveRecord::Migration[7.0]
+class CreatePaySubscriptionItems < ActiveRecord::Migration[6.0]
   def change
     create_table :pay_subscription_items do |t|
       t.references :pay_subscription
