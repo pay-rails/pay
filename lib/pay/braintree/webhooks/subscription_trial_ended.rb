9619524719b449abb073d39d916af9f51ceaf388
Collin Jilbert <cjilbert504@gmail.com>
2022-03-24 16:14:02 -0500

    call update!

--- lib/pay/braintree/webhooks/subscription_trial_ended.rb
+++ lib/pay/braintree/webhooks/subscription_trial_ended.rb
@@ -11,7 +11,7 @@ module Pay
           pay_subscription = Pay::Subscription.find_by_processor_and_id(:braintree, subscription.id)
           return unless pay_subscription.present?
 
-          pay_subscription.update(trial_ends_at: Time.current)
+          pay_subscription.update!(trial_ends_at: Time.current)
         end
       end
     end
