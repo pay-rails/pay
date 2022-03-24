f8e3998587f4f14b729188adde69b125adc3ffa9
Collin Jilbert <cjilbert504@gmail.com>
2022-03-24 14:50:00 -0500

    update to pay_charge:

--- lib/pay/stripe/webhooks/charge_succeeded.rb
+++ lib/pay/stripe/webhooks/charge_succeeded.rb
@@ -6,7 +6,7 @@ module Pay
           pay_charge = Pay::Stripe::Charge.sync(event.data.object.id, stripe_account: event.try(:account))
 
           if pay_charge && Pay.send_emails
-            Pay::UserMailer.with(pay_customer: pay_charge.customer, charge: pay_charge).receipt.deliver_later
+            Pay::UserMailer.with(pay_customer: pay_charge.customer, pay_charge: pay_charge).receipt.deliver_later
           end
         end
       end
