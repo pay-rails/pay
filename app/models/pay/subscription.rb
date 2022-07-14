6defacea03396ccfaeed6b8c31832de076ce71d2
Collin Jilbert <cjilbert504@gmail.com>
2022-07-14 17:06:49 -0500

    use json1 ext syntax

--- app/models/pay/subscription.rb
+++ app/models/pay/subscription.rb
@@ -12,7 +12,7 @@ module Pay
     scope :cancelled, -> { where.not(ends_at: nil) }
     scope :on_grace_period, -> { cancelled.where("#{table_name}.ends_at > ?", Time.zone.now) }
     scope :active, -> { where(status: ["trialing", "active"], ends_at: nil).or(on_grace_period).or(on_trial) }
-    scope :not_paused, -> { where("data->>'pause_behavior' IS NULL") }
+    scope :not_paused, -> { where("json_extract(pay_subscriptions.data, '$.pause_behavior') IS NULL") }
     scope :incomplete, -> { where(status: :incomplete) }
     scope :past_due, -> { where(status: :past_due) }
     scope :with_active_customer, -> { joins(:customer).merge(Customer.active) }
