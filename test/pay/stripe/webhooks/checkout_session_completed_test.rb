51183145b3b6190e2176413c7f01c0f9df08d464
Chris Oliver <excid3@gmail.com>
2022-07-27 09:43:24 -0500

    Fix test

--- test/pay/stripe/webhooks/checkout_session_completed_test.rb
+++ test/pay/stripe/webhooks/checkout_session_completed_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Pay::Stripe::Webhooks::CheckoutSessionCompletedTest < ActiveSupport::TestCase
   test "creates Pay::Customer if client_reference_id present and valid" do
-    event = stripe_event("checkout.session.completed", overrides: { "object" => { "client_reference_id" => users(:none).to_sgid.to_s }})
+    event = stripe_event("checkout.session.completed", overrides: {"object" => {"client_reference_id" => users(:none).to_sgid.to_s}})
     Pay::Stripe::Subscription.expects(:sync)
     assert_difference "Pay::Customer.count" do
       Pay::Stripe::Webhooks::CheckoutSessionCompleted.new.call(event)
@@ -10,7 +10,7 @@ class Pay::Stripe::Webhooks::CheckoutSessionCompletedTest < ActiveSupport::TestC
   end
 
   test "handles client_reference_id if present but not valid" do
-    event = stripe_event("checkout.session.completed", overrides: { object: { client_reference_id: users(:none).to_sgid.to_s }})
+    event = stripe_event("checkout.session.completed", overrides: {"object" => {"client_reference_id" => "invalid"}})
     Pay::Stripe::Subscription.expects(:sync)
     assert_no_difference "Pay::Customer.count" do
       Pay::Stripe::Webhooks::CheckoutSessionCompleted.new.call(event)
