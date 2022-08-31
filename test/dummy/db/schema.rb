6dcdd476ae7ad6af01f5e9c1a48c2bee683ed48e
Chris Oliver <excid3@gmail.com>
2022-08-31 15:30:54 -0500

    Remove version from schema

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.0].define(version: 2022_08_31_153001) do
+ActiveRecord::Schema.define(version: 2022_08_31_153001) do
   create_table "accounts", force: :cascade do |t|
     t.string "email"
     t.string "merchant_processor"
