bf5ea10eced14b227af85b178a2bbdb8e0414b74
Alex Filatov <alex@alexfilatov.com>
2022-11-10 09:50:40 +0000

    fixed from_address

--- app/mailers/pay/application_mailer.rb
+++ app/mailers/pay/application_mailer.rb
@@ -1,14 +1,14 @@
 module Pay
   class ApplicationMailer < ActionMailer::Base
-    default from: from(Pay.business_name, Pay.support_email) || ApplicationMailer.default_params[:from]
-    layout "mailer"
+    def from_address(name, email)
+      return if email.blank?
 
-    def from(name, email)
-      return if email.blank? 
-      
       address = Mail::Address.new email
       address.display_name = name if name.present?
       address.format
     end
+
+    default from: from_address(Pay.business_name, Pay.support_email) || ApplicationMailer.default_params[:from]
+    layout "mailer"
   end
 end
