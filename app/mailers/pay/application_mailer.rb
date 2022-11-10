1359871f9eaaf307cc081125f1625c9bc876309c
Alex Filatov <alex@alexfilatov.com>
2022-11-10 09:35:18 +0000

    added from method to Pay::ApplicationMailer

--- app/mailers/pay/application_mailer.rb
+++ app/mailers/pay/application_mailer.rb
@@ -1,6 +1,14 @@
 module Pay
   class ApplicationMailer < ActionMailer::Base
-    default from: Pay.support_email || ApplicationMailer.default_params[:from]
+    default from: from(Pay.business_name, Pay.support_email) || ApplicationMailer.default_params[:from]
     layout "mailer"
+
+    def from(name, email)
+      return if email.blank? 
+      
+      address = Mail::Address.new email
+      address.display_name = name if name.present?
+      address.format
+    end
   end
 end
