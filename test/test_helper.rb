187492dbd3248d3c6a9d3a32dac069afc5d5c21e
Chris Oliver <excid3@gmail.com>
2023-03-21 16:43:02 -0500

    Re-add IntegrationTest fixture path for older Rails

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -39,6 +39,7 @@ if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActiveSupport::TestCase.fixture_paths << File.expand_path("../fixtures", __FILE__)
 elsif ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
 end
 
 ActiveSupport::TestCase.fixtures :all
