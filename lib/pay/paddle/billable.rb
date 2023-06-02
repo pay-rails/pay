22239744cc7c374a54a4934fa74f07598a824f89
Collin Jilbert <cjilbert504@gmail.com>
2023-06-02 09:55:13 -0500

    supply default value for add_payment_method token arg; add test for adding/updating payment methods

--- lib/pay/paddle/billable.rb
+++ lib/pay/paddle/billable.rb
@@ -49,7 +49,9 @@ module Pay
         # pass
       end
 
-      def add_payment_method(token, default: true)
+      # Paddle does not use payment method tokens. The method signature has it here
+      # to have a uniform API with the other payment processors.
+      def add_payment_method(token = nil, default: true)
         Pay::Paddle::PaymentMethod.sync(pay_customer: pay_customer)
       end
 
