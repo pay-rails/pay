87bdac4e2c9f40ea373ef6fbd9599f7a50268010
Chris Oliver <excid3@gmail.com>
2023-07-18 14:36:15 -0500

    Fix integration test fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -37,6 +37,7 @@ Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActiveSupport::TestCase.fixture_paths << File.expand_path("../fixtures", __FILE__)
+  ActionDispatch::IntegrationTest.fixture_paths << File.expand_path("../fixtures", __FILE__)
 elsif ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
