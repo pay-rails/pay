ed14c131cdfed8f4aa9bf8f97457a522f9f34553
Chris Oliver <excid3@gmail.com>
2023-07-18 11:22:59 -0500

    Better mock env

--- test/pay/stripe_test.rb
+++ test/pay/stripe_test.rb
@@ -2,13 +2,18 @@ require "test_helper"
 
 class Pay::Stripe::Test < ActiveSupport::TestCase
   test "finds API keys from env" do
-    ENV["STRIPE_PUBLIC_KEY"] = "public"
-    ENV["STRIPE_PRIVATE_KEY"] = "private"
-    ENV["STRIPE_SIGNING_SECRET"] = "secret"
+    old_env = ENV.to_hash
+    ENV.update(
+      "STRIPE_PUBLIC_KEY" => "public",
+      "STRIPE_PRIVATE_KEY" => "private",
+      "STRIPE_SIGNING_SECRET" => "secret"
+    )
 
     assert_equal "public", Pay::Stripe.public_key
     assert_equal "private", Pay::Stripe.private_key
     assert_equal "secret", Pay::Stripe.signing_secret
+  ensure
+    ENV.update(old_env)
   end
 
   test "can generate a client_reference_id for a model" do
