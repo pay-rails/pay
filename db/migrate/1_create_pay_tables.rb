8cc7ed9e014c327d304a5ba4277a1b4fbb3d7ef2
Matthias Schmidt <mschmidt@pixelflush.com>
2023-08-09 17:42:25 +0200

    Standard format

--- db/migrate/1_create_pay_tables.rb
+++ db/migrate/1_create_pay_tables.rb
@@ -81,11 +81,12 @@ class CreatePayTables < ActiveRecord::Migration[6.0]
   end
 
   private
-    def primary_and_foreign_key_types
-      config = Rails.configuration.generators
-      setting = config.options[config.orm][:primary_key_type]
-      primary_key_type = setting || :primary_key
-      foreign_key_type = setting || :bigint
-      [primary_key_type, foreign_key_type]
-    end
+
+  def primary_and_foreign_key_types
+    config = Rails.configuration.generators
+    setting = config.options[config.orm][:primary_key_type]
+    primary_key_type = setting || :primary_key
+    foreign_key_type = setting || :bigint
+    [primary_key_type, foreign_key_type]
+  end
 end
