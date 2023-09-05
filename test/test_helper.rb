930ec5e2db3602a1c02ba3afc8b88ed748d6eda9
Dean Perry <deanperry@fastmail.com>
2023-09-05 15:31:19 +0100

    require openssl and base64

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,8 @@ ENV["STRIPE_PRIVATE_KEY"] ||= "sk_test_fake"
 ENV["STRIPE_SIGNING_SECRET"] ||= "whsec_x"
 
 # Paddle Classic configuration
+require "openssl"
+require "base64"
 paddle_public_key = OpenSSL::PKey::RSA.new(File.read("test/support/fixtures/paddle_classic/verification/paddle_public_key.pem"))
 ENV["PADDLE_PUBLIC_KEY_BASE64"] = Base64.encode64(paddle_public_key.to_der)
 ENV["PADDLE_ENVIRONMENT"] = "sandbox"
