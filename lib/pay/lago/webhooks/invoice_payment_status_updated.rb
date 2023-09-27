9c25fb59b3a0359472cad8a3588905c8bb2425e4
Moxvallix <moxvallix@gmail.com>
2023-09-27 15:27:48 +0930

    fix: webhook lago.invoice.payment_status_updated now works

--- lib/pay/lago/webhooks/invoice_payment_status_updated.rb
+++ lib/pay/lago/webhooks/invoice_payment_status_updated.rb
@@ -3,7 +3,9 @@ module Pay
     module Webhooks
       class InvoicePaymentStatusUpdated
         def call(event)
-          Pay::Lago::Charge.sync(event.invoice.lago_id, object: event.invoice)
+          charge = Pay::Charge.find_by_processor_and_id(:lago, event.invoice.lago_id)
+          charge.data.merge!(payment_status: event.invoice.payment_status)
+          charge.save!
         end
       end
     end
