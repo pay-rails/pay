a1ed86b5b28943c5f93fe1b8263de89164876461
Dean Perry <deanperry@fastmail.com>
2023-10-21 17:38:18 +0100

    standardrb fix

--- lib/pay/paddle/billable.rb
+++ lib/pay/paddle/billable.rb
@@ -46,7 +46,7 @@ module Pay
 
       def charge(amount, options = {})
         return Pay::Error unless options
-        
+
         items = options[:items]
         opts = options.except(:items).merge(customer_id: processor_id)
         transaction = ::Paddle::Transaction.create(items: items, **opts)
