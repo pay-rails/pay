91be0feebb1e72d805511cfb699e1111461fc196
Dean Perry <deanperry@fastmail.com>
2023-10-26 18:35:00 +0100

    test retrieving billing subscription

--- test/pay/paddle_billing/billable_test.rb
+++ test/pay/paddle_billing/billable_test.rb
@@ -8,4 +8,9 @@ class Pay::PaddleBilling::Billable::Test < ActiveSupport::TestCase
   test "paddle cannot create a charge without options" do
     assert_raises(Pay::Error) { @pay_customer.charge(1000) }
   end
+
+  test "retrieving a paddle billing subscription" do
+    subscription = ::Paddle::Subscription.retrieve(id: "sub_01hd1drf5htjz45yt2346anmqt")
+    assert_equal @pay_customer.processor_subscription("sub_01hd1drf5htjz45yt2346anmqt").id, subscription.id
+  end
 end
