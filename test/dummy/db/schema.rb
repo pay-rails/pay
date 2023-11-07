269774445eef26fe8e76ab810d8c6c318584fa88
Dean Perry <deanperry@fastmail.com>
2023-11-07 17:18:24 +0000

    revert schema

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2017_02_05_000000) do
+ActiveRecord::Schema.define(version: 2022_08_31_153001) do
   create_table "accounts", force: :cascade do |t|
     t.string "email"
     t.string "merchant_processor"
@@ -21,10 +21,10 @@ ActiveRecord::Schema[7.1].define(version: 2017_02_05_000000) do
     t.bigint "customer_id", null: false
     t.bigint "subscription_id"
     t.string "processor_id", null: false
-    t.integer "amount", null: false
+    t.bigint "amount", null: false
     t.string "currency"
-    t.integer "application_fee_amount"
-    t.integer "amount_refunded"
+    t.bigint "application_fee_amount"
+    t.bigint "amount_refunded"
     t.json "metadata"
     t.json "data"
     t.string "stripe_account"
@@ -42,10 +42,10 @@ ActiveRecord::Schema[7.1].define(version: 2017_02_05_000000) do
     t.boolean "default"
     t.json "data"
     t.string "stripe_account"
-    t.datetime "deleted_at", precision: nil
+    t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["owner_type", "owner_id", "deleted_at"], name: "pay_customer_owner_index", unique: true
+    t.index ["owner_type", "owner_id", "deleted_at", "default"], name: "pay_customer_owner_index"
     t.index ["processor", "processor_id"], name: "index_pay_customers_on_processor_and_processor_id", unique: true
   end
 
@@ -78,23 +78,23 @@ ActiveRecord::Schema[7.1].define(version: 2017_02_05_000000) do
     t.string "name", null: false
     t.string "processor_id", null: false
     t.string "processor_plan", null: false
-    t.integer "quantity", default: 1, null: false
+    t.bigint "quantity", default: 1, null: false
     t.string "status", null: false
-    t.datetime "current_period_start", precision: nil
-    t.datetime "current_period_end", precision: nil
-    t.datetime "trial_ends_at", precision: nil
-    t.datetime "ends_at", precision: nil
-    t.boolean "metered"
-    t.string "pause_behavior"
-    t.datetime "pause_starts_at", precision: nil
-    t.datetime "pause_resumes_at", precision: nil
+    t.datetime "current_period_start"
+    t.datetime "current_period_end"
+    t.datetime "trial_ends_at"
+    t.datetime "ends_at"
     t.decimal "application_fee_percent", precision: 8, scale: 2
     t.json "metadata"
     t.json "data"
     t.string "stripe_account"
+    t.datetime "created_at", null: false, precision: 6
+    t.datetime "updated_at", null: false, precision: 6
+    t.boolean "metered"
+    t.string "pause_behavior"
+    t.datetime "pause_starts_at"
+    t.datetime "pause_resumes_at"
     t.string "payment_method_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["customer_id", "processor_id"], name: "index_pay_subscriptions_on_customer_id_and_processor_id", unique: true
     t.index ["metered"], name: "index_pay_subscriptions_on_metered"
     t.index ["pause_starts_at"], name: "index_pay_subscriptions_on_pause_starts_at"
@@ -112,7 +112,7 @@ ActiveRecord::Schema[7.1].define(version: 2017_02_05_000000) do
     t.string "email"
     t.string "name"
     t.string "owner_type"
-    t.integer "owner_id"
+    t.bigint "owner_id"
     t.index ["owner_type", "owner_id"], name: "index_teams_on_owner_type_and_owner_id"
   end
 
