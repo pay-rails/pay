6d602b29dd3f7e11b9a011d4c8e34cab8bce70a0
Chris Oliver <excid3@gmail.com>
2024-01-10 09:57:24 -0600

    Remove subscription item ID from options when creating or listing usage records

--- lib/pay/stripe/subscription.rb
+++ lib/pay/stripe/subscription.rb
@@ -342,13 +342,13 @@ module Pay
       # create_usage_record(quantity: 4, action: :increment)
       # create_usage_record(subscription_item_id: "si_1234", quantity: 100, action: :set)
       def create_usage_record(**options)
-        subscription_item_id = options.fetch(:subscription_item_id, metered_subscription_item&.dig("id"))
+        subscription_item_id = options.delete(:subscription_item_id) || metered_subscription_item&.dig("id")
         ::Stripe::SubscriptionItem.create_usage_record(subscription_item_id, options, stripe_options)
       end
 
       # Returns usage record summaries for a subscription item
       def usage_record_summaries(**options)
-        subscription_item_id = options.fetch(:subscription_item_id, metered_subscription_item&.dig("id"))
+        subscription_item_id = options.delete(:subscription_item_id) || metered_subscription_item&.dig("id")
         ::Stripe::SubscriptionItem.list_usage_record_summaries(subscription_item_id, options, stripe_options)
       end
 
