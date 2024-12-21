9ca1f6c3d9a82cc93b6b8dc4fba289fd96642cb8
Andre Arko <andre@arko.net>
2024-12-21 12:09:18 -0700

    add error cases to AWS Customer

--- app/models/pay/aws_marketplace/customer.rb
+++ app/models/pay/aws_marketplace/customer.rb
@@ -6,107 +6,32 @@ module Pay
       has_many :payment_methods, dependent: :destroy, class_name: "Pay::AwsMarketplace::PaymentMethod"
       has_one :default_payment_method, -> { where(default: true) }, class_name: "Pay::AwsMarketplace::PaymentMethod"
 
+      store_accessor :data, :aws_account_id
+
       def api_record
-        update!(processor_id: NanoId.generate) unless processor_id?
-        self
+        {customer_identifier: processor_id, customer_aws_account_id: aws_account_id}.compact
       end
 
       def update_api_record(**attributes)
-        self
+        raise UpdateError, "AWS Marketplace does not allow updating customer information"
       end
 
       def charge(amount, options = {})
-        raise Pay::Error, "AWS Marketplace does not support one-off charges"
+        raise ChargeError, "AWS Marketplace does not support one-off charges"
       end
 
-      # https://stripe.com/docs/api/checkout/sessions/create
-      #
-      # checkout(mode: "payment")
-      # checkout(mode: "setup")
-      # checkout(mode: "subscription")
-      #
-      # checkout(line_items: "price_12345", quantity: 2)
-      # checkout(line_items: [{ price: "price_123" }, { price: "price_456" }])
-      # checkout(line_items: "price_12345", allow_promotion_codes: true)
-      #
-      def checkout(**options)
-        api_record unless processor_id?
-        args = {
-          customer: processor_id,
-          mode: "payment"
-        }
-
-        # Hosted (the default) checkout sessions require a success_url and cancel_url
-        if ["", "hosted"].include? options[:ui_mode].to_s
-          args[:success_url] = merge_session_id_param(options.delete(:success_url) || root_url)
-          args[:cancel_url] = merge_session_id_param(options.delete(:cancel_url) || root_url)
-        end
-
-        if options[:return_url]
-          args[:return_url] = merge_session_id_param(options.delete(:return_url))
-        end
-
-        # Line items are optional
-        if (line_items = options.delete(:line_items))
-          quantity = options.delete(:quantity) || 1
-
-          args[:line_items] = Array.wrap(line_items).map { |item|
-            if item.is_a? Hash
-              item
-            else
-              {
-                price: item,
-                quantity: quantity
-              }
-            end
-          }
-        end
-
-        ::Stripe::Checkout::Session.create(args.merge(options), stripe_options)
+      def checkout(product_id:)
+        URI("https://aws.amazon.com/marketplace/procurement").tap do |url|
+          url.query = URI.encode_www_form(productId: product_id)
+        end.to_s
       end
 
       def subscribe(name: Pay.default_product_name, plan: Pay.default_plan_name, **options)
-        # Make to generate a processor_id
-        api_record
-        attributes = options.merge(
-          processor_id: NanoId.generate,
-          name: name,
-          processor_plan: plan,
-          status: :active,
-          quantity: options.fetch(:quantity, 1)
-        )
-
-        if (trial_period_days = attributes.delete(:trial_period_days))
-          attributes[:trial_ends_at] = trial_period_days.to_i.days.from_now
-        end
-
-        attributes.delete(:promotion_code)
-
-        subscriptions.create!(attributes)
+        raise UpdateError
       end
 
       def add_payment_method(payment_method_id, default: false)
-        # Make to generate a processor_id
-        api_record
-
-        pay_payment_method = payment_methods.create!(
-          processor_id: NanoId.generate,
-          default: default,
-          payment_method_type: :card,
-          data: {
-            brand: "AWS",
-            last4: 1234,
-            exp_month: Date.today.month,
-            exp_year: Date.today.year
-          }
-        )
-
-        if default
-          payment_methods.where.not(id: pay_payment_method.id).update_all(default: false)
-          reload_default_payment_method
-        end
-
-        pay_payment_method
+        raise PaymentMethodError
       end
     end
   end
--- test/pay/aws_marketplace/customer_test.rb
+++ test/pay/aws_marketplace/customer_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Pay::AwsMarketplace::CustomerTest < ActiveSupport::TestCase
   setup do
-    @pay_customer = pay_customers(:fake)
+    @pay_customer = pay_customers(:aws_marketplace)
   end
 
   test "allows aws_marketplace processor" do
@@ -12,53 +12,51 @@ class Pay::AwsMarketplace::CustomerTest < ActiveSupport::TestCase
   end
 
   test "aws processor api_record" do
-    assert_equal @pay_customer, @pay_customer.api_record
+    assert_equal @pay_customer.api_record, {customer_identifier: "QzOTBiMmRmN"}
   end
 
   test "aws processor charge" do
-    assert_difference "Pay::Charge.count" do
+    assert_raises Pay::AwsMarketplace::ChargeError do
       @pay_customer.charge(10_00)
     end
   end
 
-  test "aws processor charge options" do
-    assert_difference "Pay::Charge.count" do
-      @pay_customer.charge(10_00, {description: "Hello world"})
-    end
-  end
-
   test "aws processor subscribe" do
-    assert_difference "Pay::Subscription.count" do
+    assert_raises Pay::AwsMarketplace::UpdateError do
       @pay_customer.subscribe
     end
   end
 
   test "aws processor subscribe with promotion code" do
-    assert_difference "Pay::Subscription.count" do
+    assert_raises Pay::AwsMarketplace::UpdateError do
       @pay_customer.subscribe(promotion_code: "promo_xxx123")
     end
   end
 
   test "aws processor add new default payment method" do
-    old_payment_method = @pay_customer.add_payment_method("old", default: true)
-    assert_equal old_payment_method.id, @pay_customer.default_payment_method.id
-
-    new_payment_method = nil
-    assert_difference "Pay::PaymentMethod.count" do
-      new_payment_method = @pay_customer.add_payment_method("new", default: true)
+    assert_raises Pay::AwsMarketplace::PaymentMethodError do
+      @pay_customer.add_payment_method("new", default: true)
     end
-
-    payment_method = @pay_customer.default_payment_method
-    assert_equal new_payment_method.id, payment_method.id
-    assert_equal "card", payment_method.payment_method_type
-    assert_equal "Fake", payment_method.brand
   end
 
-  test "generates aws processor_id" do
+  test "aws customer stores aws account id" do
     user = users(:none)
     pay_customer = user.set_payment_processor :aws_marketplace
     assert_nil pay_customer.processor_id
-    pay_customer.api_record
+    assert_nil pay_customer.aws_account_id
+    pay_customer.update!(processor_id: "bwhUQyJL8sd", aws_account_id: "25404655876")
     assert_not_nil pay_customer.processor_id
+    assert_not_nil pay_customer.aws_account_id
+  end
+
+  test "aws customer update api record" do
+    user = users(:none)
+    pay_customer = user.set_payment_processor :aws_marketplace
+
+    assert_raises Pay::AwsMarketplace::UpdateError do
+      pay_customer.update_api_record(
+        "customer_identifier" => "bwhUQyJL8sd", "customer_aws_account_id" => "25404655876"
+      )
+    end
   end
 end
