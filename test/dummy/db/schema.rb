600a5dda1489463ea5883ee3e095c0c7031cc365
Chris Oliver <excid3@gmail.com>
2025-04-18 13:12:58 -0500

    Update schema version

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_15_151129) do
+ActiveRecord::Schema[6.0].define(version: 2025_04_15_151129) do
   create_table "accounts", force: :cascade do |t|
     t.string "email"
     t.string "merchant_processor"
