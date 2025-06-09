357c46ae664586e9ff5cb79c855b87dad0927259
Collin Jilbert <cjilbert504@gmail.com>
2025-06-09 11:04:08 -0500

    standardize

--- test/mailers/pay/user_mailer_test.rb
+++ test/mailers/pay/user_mailer_test.rb
@@ -1,35 +1,35 @@
-require 'test_helper'
+require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
   setup do
     @charge = pay_charges(:stripe)
     @pay_customer = @charge.customer
     @user = @pay_customer.owner
-    @user.update(extra_billing_info: 'extra billing info')
+    @user.update(extra_billing_info: "extra billing info")
 
-    @charge.update stripe_invoice: JSON.parse(file_fixture('stripe/invoice.json').read)
+    @charge.update stripe_invoice: JSON.parse(file_fixture("stripe/invoice.json").read)
   end
 
-  test 'from address with Pay.support_email' do
-    Pay.stub :support_email, 'pay@test.org' do
-      assert_equal 'pay@test.org', Pay::ApplicationMailer.default_from_address
+  test "from address with Pay.support_email" do
+    Pay.stub :support_email, "pay@test.org" do
+      assert_equal "pay@test.org", Pay::ApplicationMailer.default_from_address
     end
   end
 
-  test 'from address fallback to ::ApplicationMailer.default_params' do
+  test "from address fallback to ::ApplicationMailer.default_params" do
     Pay.stub :support_email, nil do
-      assert_equal 'from@example.com', Pay::ApplicationMailer.default_from_address
+      assert_equal "from@example.com", Pay::ApplicationMailer.default_from_address
     end
   end
 
-  test 'receipt' do
+  test "receipt" do
     email = Pay::UserMailer.with(pay_customer: @pay_customer, pay_charge: @charge).receipt
 
     assert_equal [@user.email], email.to
-    assert_equal I18n.t('pay.user_mailer.receipt.subject', application: Pay.application_name), email.subject
+    assert_equal I18n.t("pay.user_mailer.receipt.subject", application: Pay.application_name), email.subject
   end
 
-  test 'render receipt with missing support email' do
+  test "render receipt with missing support email" do
     Pay.stub :support_email, nil do
       assert_nothing_raised do
         @charge.receipt
@@ -37,11 +37,11 @@ class UserMailerTest < ActionMailer::TestCase
     end
   end
 
-  test 'attaches refunds to receipt' do
-    filename = 'receipt.pdf'
+  test "attaches refunds to receipt" do
+    filename = "receipt.pdf"
 
-    receipt = mock('receipt')
-    receipt.stubs(:render).returns('render content')
+    receipt = mock("receipt")
+    receipt.stubs(:render).returns("render content")
     receipt.stubs(:length).returns(1024)
 
     @charge.stubs(:filename).returns(filename)
@@ -52,70 +52,70 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal filename, email.attachments.first.filename
   end
 
-  test 'refund' do
+  test "refund" do
     email = Pay::UserMailer.with(pay_customer: @pay_customer, pay_charge: @charge).refund
 
     assert_equal [@user.email], email.to
-    assert_equal I18n.t('pay.user_mailer.refund.subject', application: Pay.application_name), email.subject
+    assert_equal I18n.t("pay.user_mailer.refund.subject", application: Pay.application_name), email.subject
   end
 
-  test 'subscription_renewing' do
+  test "subscription_renewing" do
     time = Time.current
     email = Pay::UserMailer.with(pay_customer: @pay_customer, pay_subscription: Pay::Subscription.new, date: time).subscription_renewing
 
     assert_equal [@user.email], email.to
-    assert_equal I18n.t('pay.user_mailer.subscription_renewing.subject', application: Pay.application_name),
-                 email.subject
+    assert_equal I18n.t("pay.user_mailer.subscription_renewing.subject", application: Pay.application_name),
+      email.subject
     assert_includes email.html_part.decoded, I18n.l(time.to_date, format: :long)
   end
 
-  test 'payment_action_required' do
-    email = Pay::UserMailer.with(pay_customer: @pay_customer, payment_intent_id: 'x', pay_subscription: Pay::Subscription.new).payment_action_required
+  test "payment_action_required" do
+    email = Pay::UserMailer.with(pay_customer: @pay_customer, payment_intent_id: "x", pay_subscription: Pay::Subscription.new).payment_action_required
 
     assert_equal [@user.email], email.to
-    assert_equal I18n.t('pay.user_mailer.payment_action_required.subject', application: Pay.application_name), email.subject
-    assert_includes email.html_part.decoded, Pay::Engine.instance.routes.url_helpers.payment_path('x')
+    assert_equal I18n.t("pay.user_mailer.payment_action_required.subject", application: Pay.application_name), email.subject
+    assert_includes email.html_part.decoded, Pay::Engine.instance.routes.url_helpers.payment_path("x")
   end
 
-  test 'receipt with no extra billing info column' do
+  test "receipt with no extra billing info column" do
     team = teams(:one)
     @pay_customer.update!(owner: team)
     email = Pay::UserMailer.with(pay_customer: @pay_customer, pay_charge: @charge).receipt
 
     assert_equal [team.owner.email], email.to
-    assert_equal I18n.t('pay.user_mailer.receipt.subject', application: Pay.application_name), email.subject
+    assert_equal I18n.t("pay.user_mailer.receipt.subject", application: Pay.application_name), email.subject
   end
 
-  test 'refund with no extra billing info column' do
+  test "refund with no extra billing info column" do
     team = teams(:one)
     @pay_customer.update!(owner: team)
     email = Pay::UserMailer.with(pay_customer: @pay_customer, pay_charge: @charge).refund
 
     assert_equal [team.owner.email], email.to
-    assert_equal I18n.t('pay.user_mailer.refund.subject', application: Pay.application_name), email.subject
+    assert_equal I18n.t("pay.user_mailer.refund.subject", application: Pay.application_name), email.subject
   end
 
-  test 'subscription_trial_will_end' do
+  test "subscription_trial_will_end" do
     email = Pay::UserMailer.with(pay_customer: @pay_customer).subscription_trial_will_end
 
     assert_equal [@user.email], email.to
-    assert_equal I18n.t('pay.user_mailer.subscription_trial_will_end.subject', application: Pay.application_name), email.subject
-    assert_includes email.html_part.decoded, 'trial is ending soon'
+    assert_equal I18n.t("pay.user_mailer.subscription_trial_will_end.subject", application: Pay.application_name), email.subject
+    assert_includes email.html_part.decoded, "trial is ending soon"
   end
 
-  test 'subscription_trial_ended' do
+  test "subscription_trial_ended" do
     email = Pay::UserMailer.with(pay_customer: @pay_customer).subscription_trial_ended
 
     assert_equal [@user.email], email.to
-    assert_equal I18n.t('pay.user_mailer.subscription_trial_ended.subject', application: Pay.application_name), email.subject
-    assert_includes email.html_part.decoded, 'trial has ended'
+    assert_equal I18n.t("pay.user_mailer.subscription_trial_ended.subject", application: Pay.application_name), email.subject
+    assert_includes email.html_part.decoded, "trial has ended"
   end
 
-  test 'subscription payment failed' do
+  test "subscription payment failed" do
     email = Pay::UserMailer.with(pay_customer: @pay_customer).payment_failed
 
     assert_equal [@user.email], email.to
-    assert_equal I18n.t('pay.user_mailer.payment_failed.subject', application: Pay.application_name), email.subject
-    assert_includes email.html_part.decoded, 'payment was declined'
+    assert_equal I18n.t("pay.user_mailer.payment_failed.subject", application: Pay.application_name), email.subject
+    assert_includes email.html_part.decoded, "payment was declined"
   end
 end
